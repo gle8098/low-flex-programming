a107e44fff8fc9e7a5820089d83d9fe191379118
supercomputer <gle8098@yandex.ru>
2020-12-03 01:15:40 +0300

    Draft for terminal buffer

--- vga.c
+++ vga.c
@@ -4,10 +4,17 @@
 
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
+static const size_t BUFFER_HEIGHT = 200;
+
+#define VGA_INDEX(x, y) ((y) * VGA_WIDTH + (x))
 
 static size_t terminal_row;
 static size_t terminal_column;
-static uint16_t* terminal_buffer;
+static uint16_t terminal_buffer[200 * 80];
+
+static uint16_t* terminal_out;
+static size_t terminal_scroll;
+static int terminal_autoscroll;
 
 void handle_keypress(keyboard_event_t* event, void* _) {
     char str[] = {event->keycode, '\0'};
@@ -17,12 +24,15 @@ void handle_keypress(keyboard_event_t* event, void* _) {
 void terminal_initialize(void) {
     terminal_row = 0;
     terminal_column = 0;
+    terminal_scroll = 0;
+    memset(terminal_buffer, '\0', sizeof(terminal_buffer));
+
     uint8_t color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    terminal_buffer = phys2virt((uint16_t*) 0xB8000);
+    terminal_out = phys2virt((uint16_t*) 0xB8000);
     for (size_t y = 0; y < VGA_HEIGHT; y++) {
         for (size_t x = 0; x < VGA_WIDTH; x++) {
             const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = vga_entry(' ', color);
+            terminal_out[index] = vga_entry(' ', color);
         }
     }
 
@@ -30,8 +40,23 @@ void terminal_initialize(void) {
 }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
-    const size_t index = y * VGA_WIDTH + x;
-    terminal_buffer[index] = vga_entry(c, color);
+    const size_t index = VGA_INDEX(x, y);
+    const uint16_t entry = vga_entry(c, color);
+    terminal_buffer[index] = entry;
+
+    if (terminal_scroll <= y && y <= terminal_scroll + VGA_HEIGHT) {
+        terminal_out[VGA_INDEX(x, y - terminal_scroll)] = entry;
+    }
+}
+
+void terminal_shift_buffer() {
+    for (size_t y = )
+}
+
+void terminal_newline_buffer() {
+    if (terminal_row == BUFFER_HEIGHT) {
+        terminal_shift_buffer();
+    }
 }
 
 void terminal_putchar_color(char c, uint8_t color) {
